4f739d67d0ad82f7b7fc6866265d56742021e36f
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-11-02 09:02:00 +0100

    still no changes

--- admin/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
-
-from . import admin_login
-from . import create_candidate
-from . import create_voter
-from . import register_admin_user
