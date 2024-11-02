ec60c355532f3d109bdc29c084a9bd285b72ef6f
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-11-02 08:49:03 +0100

    git pulled and did not find anything

--- run.py
+++
@@ -1,6 +0,0 @@
-from ALX_FINAL_PROJECT import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
--- voters/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-voters_bp = Blueprint('voters', __name__, url_prefix='/voter')
-
-from . import voters_login
